67c272981b18a37c2a03842560a9ab0e4c9baed3
robinslevel <56522731+robinslevel@users.noreply.github.com>
2020-11-23 10:25:02 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+tofflebutton.addEventListener('click',  () =>{
+  navbarlinks.classList.toggle('active')
+})
